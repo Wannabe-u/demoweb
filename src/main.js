f6d03cfe571c95e63ef5bbb198095571a4940c0a
Wannabe-u <1103080854@qq.com>
2020-04-23 17:39:07 +0800

    20200423

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import './style/theme.css'
+import './style/characters.css'
+
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
